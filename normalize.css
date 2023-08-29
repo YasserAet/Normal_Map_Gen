6f3564ebe8e8f6c408df60b3b53704165c23a77b
Aitali Yasser <52467049+YasserAet@users.noreply.github.com>
2023-08-29 13:46:47 +0100

    Create normalize.css

---
+++ normalize.css
@@ -0,0 +1,50 @@
+html {
+    margin: 0px;
+    padding: 0px;
+    width: 100vw;
+    height: 100vh;
+  }
+  body {
+    margin: 0px;
+    padding: 0px;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    font-family: "Arial Unicode MS";
+    background: linear-gradient(330deg, rgb(150, 150, 150), rgb(200, 200, 200));
+    background-color: rgb(200, 200, 200);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .canvas {
+    outline: 1px solid hsla(0, 0%, 0%, 0.25);
+  }
+  .progress {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 40px;
+    display: flex;
+  }
+  .progress span {
+    width: calc(100% - 160px);
+    height: 40px;
+    line-height: 40px;
+    color: hsl(0, 0%, 0%);
+    text-align: center;
+  }
+  input[type="button"] {
+    margin: 0px;
+    width: 120px;
+    height: 40px;
+    cursor: pointer;
+    display: inline;
+  }
+  .center-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
