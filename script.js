ea1695c90e109e51bc07d8632c8c17a9f64264a8
Aitali Yasser <52467049+YasserAet@users.noreply.github.com>
2023-08-31 14:09:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,71 @@
-// Ensure you have Three.js included in your HTML file
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js"></script>
+// Initialize a Three.js scene
+const scene = new THREE.Scene();
+
+// Initialize a Three.js renderer
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+renderer.setSize(400, 400);
+
+// Append the renderer to a container in your HTML
+const canvasContainer = document.getElementById('canvas-container');
+canvasContainer.innerHTML = ''; // Clear previous content
+canvasContainer.appendChild(renderer.domElement);
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+camera.position.z = 2;
+
+// Load the image texture
+const textureLoader = new THREE.TextureLoader();
+let inputTexture;
+
+// Create a material using the image texture
+const material = new THREE.MeshBasicMaterial({ map: inputTexture });
+
+// Create a geometry (e.g., a plane) to apply the material
+const geometry = new THREE.PlaneGeometry(1, 1);
+const mesh = new THREE.Mesh(geometry, material);
+
+// Add the mesh to the scene
+scene.add(mesh);
+
+// Set up lights (you may need to adjust these for your specific needs)
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(1, 1, 1);
+scene.add(directionalLight);
 
 // Function to generate maps (occlusion, specular, and displacement)
-function generateMaps(inputImage) {
-    // Initialize a Three.js scene
-    const scene = new THREE.Scene();
-
-    // Initialize a Three.js renderer
-    const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-    renderer.setSize(400, 400);
-
-    // Append the renderer to a container in your HTML
-    const canvasContainer = document.getElementById('canvas-container');
-    canvasContainer.innerHTML = ''; // Clear previous content
-    canvasContainer.appendChild(renderer.domElement);
-
-    // Create a camera
-    const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-    camera.position.z = 2;
-
-    // Load the image texture
-    const textureLoader = new THREE.TextureLoader();
-    const texture = textureLoader.load(inputImage);
-
-    // Create a material using the image texture
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-
-    // Create a geometry (e.g., a plane) to apply the material
-    const geometry = new THREE.PlaneGeometry(1, 1);
-    const mesh = new THREE.Mesh(geometry, material);
-
-    // Add the mesh to the scene
-    scene.add(mesh);
-
-    // Set up lights (you may need to adjust these for your specific needs)
-    const ambientLight = new THREE.AmbientLight(0x404040);
-    scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-    directionalLight.position.set(1, 1, 1);
-    scene.add(directionalLight);
-
-    // Render the scene
-    const animate = () => {
-        requestAnimationFrame(animate);
-
-        // You can add animation or interaction here
-
-        renderer.render(scene, camera);
-    };
-
-    animate();
-
-    // You can perform additional map generation here, e.g., using shaders and framebuffers
+function generateMaps() {
+    // Create a renderer for off-screen rendering
+    const mapRenderer = new THREE.WebGLRenderer();
+    mapRenderer.setSize(400, 400);
+    
+    // Create render targets for each map
+    const occlusionTarget = new THREE.WebGLRenderTarget(400, 400);
+    const specularTarget = new THREE.WebGLRenderTarget(400, 400);
+    const displacementTarget = new THREE.WebGLRenderTarget(400, 400);
+    
+    // Create custom shaders for each map (you'll need to write these shaders)
+    const occlusionShader = createOcclusionShader();
+    const specularShader = createSpecularShader();
+    const displacementShader = createDisplacementShader();
+    
+    // Set the input texture for each shader
+    occlusionShader.uniforms.inputTexture.value = inputTexture;
+    specularShader.uniforms.inputTexture.value = inputTexture;
+    displacementShader.uniforms.inputTexture.value = inputTexture;
+    
+    // Render the scene to generate maps
+    mapRenderer.render(scene, camera, occlusionTarget);
+    mapRenderer.render(scene, camera, specularTarget);
+    mapRenderer.render(scene, camera, displacementTarget);
+    
+    // You can access the generated maps as textures from occlusionTarget.texture,
+    // specularTarget.texture, and displacementTarget.texture.
+    
+    // You can then use these textures in your Three.js scene or save them as images.
 }
 
 // Event listener for file input
@@ -68,8 +81,12 @@ fileInput.addEventListener('change', (event) => {
             image.src = e.target.result;
 
             image.onload = () => {
+                // Set the loaded image as the input texture
+                inputTexture = new THREE.Texture(image);
+                inputTexture.needsUpdate = true;
+
                 // Generate maps with the loaded image
-                generateMaps(image.src);
+                generateMaps();
             };
         };
 
